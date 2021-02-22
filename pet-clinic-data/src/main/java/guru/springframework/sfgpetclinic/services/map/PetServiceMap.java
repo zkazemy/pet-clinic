216cdeb4b42ea345ed35155a7d1c16834bf47b09
zkazemy <zahrakazemy@gmail.com>
2021-02-22 15:16:08 +0330

    Loan Bootstrap Data on Startup Closes#10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -3,10 +3,11 @@ package guru.springframework.sfgpetclinic.services.map;
 import guru.springframework.sfgpetclinic.Model.Owner;
 import guru.springframework.sfgpetclinic.Model.Pet;
 import guru.springframework.sfgpetclinic.services.CrudService;
+import guru.springframework.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
