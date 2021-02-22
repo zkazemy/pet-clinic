ab5809ad7561cbc742257e983c6fecd976f7462e
zkazemy <zahrakazemy@gmail.com>
2021-02-22 15:58:20 +0330

    Update Map Impl to Manage Setting of ID Property Closes #14

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,35 +1,47 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgpetclinic.Model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID>{
-    protected Map<ID,T> map = new HashMap<>();
-    Set<T> findAll()
-    {
-        return  new HashSet<>(map.values());
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
+
+    Set<T> findAll() {
+        return new HashSet<>(map.values());
     }
 
-    T findById(ID id )
-    {
+    T findById(ID id) {
         return map.get(id);
     }
 
-    T save(ID  id , T object)
-    {
-        map.put(id, object);
-    return object;
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
+        return object;
     }
 
-    void deleteById(ID id)
-    {
+    void deleteById(ID id) {
         map.remove(id);
     }
 
-    void delete(T object)
-    {
-        map.entrySet().removeIf(entry->entry.getValue().equals(object));
+    void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long maxId = null;
+        try {
+            maxId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException ex) {
+            maxId = 1L;
+        }
+        return maxId;
     }
 }
